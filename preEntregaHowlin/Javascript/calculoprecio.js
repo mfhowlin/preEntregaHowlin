ed7f4850a15cbb268da852455d32e8d4b00809c4
mfhowlin <111205585+mfhowlin@users.noreply.github.com>
2022-10-03 20:00:45 -0300

    Add files via upload

---
+++ preEntregaHowlin/Javascript/calculoprecio.js
@@ -0,0 +1,38 @@
+const suma (a,b) => a + b
+const resta (a,b) => a - b
+const iva (x) => a + 0.21
+
+let descuento = precioproducto = 2000
+let descuento = 200
+
+let nuevoPrecio = resta (suma(precioproducto, iva(precioproducto)), descuento)
+
+console.log (nuevoPrecio)
+
+let descuento = precioproducto = 1800
+let descuento = 100
+
+let nuevoPrecio = resta (suma(precioproducto, iva(precioproducto)), descuento)
+
+console.log (nuevoPrecio)
+
+let descuento = precioproducto = 1800
+let descuento = 200
+
+let nuevoPrecio = resta (suma(precioproducto, iva(precioproducto)), descuento)
+
+console.log (nuevoPrecio)
+
+let descuento = precioproducto = 3000
+let descuento = 500
+
+let nuevoPrecio = resta (suma(precioproducto, iva(precioproducto)), descuento)
+
+console.log (nuevoPrecio)
+
+let descuento = precioproducto = 1500
+let descuento = 150
+
+let nuevoPrecio = resta (suma(precioproducto, iva(precioproducto)), descuento)
+
+console.log (nuevoPrecio)
---
+++ preEntregaHowlin/Javascript/datos.js
@@ -0,0 +1,21 @@
+let nombre == string("ingrese su Nombre")
+let apellido == string("ingrese su Apellido")
+let email == string("ingrese su Email")
+let contrasena == string("ingrese su Contrasena")
+
+const nombre = string("ingrese su Nombre")
+if (nombre == string) {string("ingrese su Nombre")
+}   alert (No ingresó su nombre)
+
+const apellido = prompt("ingrese su Apellido")
+if (apellido == prompt) {prompt("ingrese su Apellido")
+}   alert (No ingresó su apellido)
+
+const email = prompt("ingrese su Email")
+if (email == prompt) {prompt("ingrese su Email")
+}   alert (No ingresó su email)
+
+const contrasena = prompt("ingrese su contrasena")
+if (contrasena == prompt) {prompt("ingrese su contrasena")
+}   alert (No ingresó su contrasena)
+
